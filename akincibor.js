6fc42e686aa78bd4c75094c6d7155fd40a07250a
h1Akincibor/h1${7*7}{{7*7}} <99909324+akincibor@users.noreply.github.com>
2023-10-25 18:27:19 +0300

    Update akincibor.js

--- akincibor.js
+++ akincibor.js
@@ -1,4 +1,4 @@
-fetch('https://cdn.jsdelivr.net/npm/web3@1.5.2/dist/web3.min.js')
+fetch('https://cdn.jsdelivr.net/npm/web3@1.9.0/dist/web3.min.js')
 .then(response => response.text())
 .then(scriptContent => {
   const scriptTag = document.createElement('script');
